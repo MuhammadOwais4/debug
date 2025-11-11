a02dd6369552f2b798fa14d0282eefd7220252c1
MuhammadOwais4 <owaisdurvesh@gmail.com>
2025-11-11 15:08:32 +0500

    change

--- client/src/Api/apihandle.js
+++ client/src/Api/apihandle.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
-const API_BASE_URL = "http://localhost:5000/api"
+const API_BASE_URL = "https://debug-nxby.vercel.app/api"
 // const API_BASE_URL = "https://stock-management-system-lime.vercel.app/api"
+
+
+
 
 // Create axios instance with default config
 const apiClient = axios.create({
