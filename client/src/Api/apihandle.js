78bef43d905c96bab8b7906abfc3c9dcbf16c44d
MuhammadOwais4 <owaisdurvesh@gmail.com>
2026-03-01 21:08:16 +0500

    chchc

--- client/src/Api/apihandle.js
+++ client/src/Api/apihandle.js
@@ -3,8 +3,6 @@ import axios from "axios"
 const API_BASE_URL = "https://debug-nxby.vercel.app/api"
 // const API_BASE_URL = "http://localhost:5000/api"
 
-
-
 // Create axios instance with default config
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
