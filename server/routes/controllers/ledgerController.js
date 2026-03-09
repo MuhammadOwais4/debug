2811463afe2ab091d95800ddad0427ee88753741
MuhammadOwais4 <owaisdurvesh@gmail.com>
2026-03-09 07:06:45 +0500

    cdajashdkaj

--- server/routes/controllers/ledgerController.js
+++ server/routes/controllers/ledgerController.js
@@ -11,7 +11,7 @@ const SaleDiscount = require("../models/Sale-discount")
 const PurchasesDiscount = require("../models/Purchases-discount")
 const { SupplierPaymentVoucher } = require("../models/Supplierpaymentvouchers")
 const { CustomerReceiptVoucher } = require("../models/CustomerReceiptVoucher")
-const OverheadVoucher = require("../models/OverheadVoucher")
+const OverheadVoucher = require("../models/Overheadcategory")
 
 // ═══════════════════════════════════════════════════════════════════════════════
 // ✅ HARDCODED TAX ACCOUNTS
